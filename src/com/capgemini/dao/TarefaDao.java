e7c8da51830b10a4bc84908d632de1203a50067f
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-06 02:13:15 -0300

    Refactory para usar try-with-resources no DAO.

--- src/com/capgemini/dao/TarefaDao.java
+++ src/com/capgemini/dao/TarefaDao.java
@@ -15,15 +15,14 @@ import com.capgemini.model.Tarefa;
 public class TarefaDao {
 	
 	public static void salvar(Tarefa tarefa) {
-			
-		Connection conexao;
-		PreparedStatement statement;
-		try { //substituir por try with resources
-			conexao = ConexaoFactory.getConnection();
-			
-			String sql = "INSERT INTO tarefa (titulo, descricao, prioridade, data_limite) VALUES (?, ?, ?, ?)";
-		 
-			statement = conexao.prepareStatement(sql);
+
+		String sql = "INSERT INTO tarefa (titulo, descricao, prioridade, data_limite) VALUES (?, ?, ?, ?)";
+		
+		try(
+				Connection conexao = ConexaoFactory.getConnection(); 
+				PreparedStatement statement = conexao.prepareStatement(sql)
+		) {
+
 			statement.setString(1, tarefa.getTitulo());
 			statement.setString(2, tarefa.getDescricao());
 			statement.setString(3, tarefa.getPrioridade());
@@ -33,6 +32,7 @@ public class TarefaDao {
 			if (linhasInseridas > 0) {
 			    System.out.println("tarefa inserida no BD!");
 			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -40,15 +40,14 @@ public class TarefaDao {
 	
 	public static List<Tarefa> getTarefas(){
 		
-		try {
+		String sql = "SELECT * FROM tarefa";
 		
-			Connection conexao = ConexaoFactory.getConnection();
-			
-			String sql = "SELECT * FROM tarefa";
-			 
-			Statement statement = conexao.createStatement();
-			ResultSet result = statement.executeQuery(sql);
-			
+		try(
+				Connection conexao = ConexaoFactory.getConnection();
+				Statement statement = conexao.createStatement();
+				ResultSet result = statement.executeQuery(sql);
+		) {
+
 			List<Tarefa> tarefas = new ArrayList<>();
 			 
 			while (result.next()){
@@ -59,7 +58,7 @@ public class TarefaDao {
 			    java.util.Date dataLimite = new java.util.Date(result.getDate(5).getTime());
 			    tarefas.add(new Tarefa(id, titulo, descricao, prioridade, dataLimite));
 			}
-			
+
 			return tarefas;
 			
 		} catch (SQLException e) {
@@ -69,13 +68,14 @@ public class TarefaDao {
 	}
 	
 	public static void apagar(Integer id) {
-		try {
 		
-			Connection conexao = ConexaoFactory.getConnection();
-			
-			String sql = "DELETE FROM tarefa WHERE id=?";
-			 
-			PreparedStatement statement = conexao.prepareStatement(sql);
+		String sql = "DELETE FROM tarefa WHERE id=?";
+		
+		try(
+				Connection conexao = ConexaoFactory.getConnection();
+				PreparedStatement statement = conexao.prepareStatement(sql);
+		) {
+
 			statement.setInt(1, id.intValue());
 			 
 			int linhasApagadas = statement.executeUpdate();
@@ -89,13 +89,13 @@ public class TarefaDao {
 	
 	public static void atualizar(Tarefa tarefa) {
 		
-		try {
+		String sql = "UPDATE tarefa SET titulo=?, descricao=?, prioridade=?, data_limite=? WHERE id=?";
 		
-			Connection conexao = ConexaoFactory.getConnection();	
-				
-			String sql = "UPDATE tarefa SET titulo=?, descricao=?, prioridade=?, data_limite=? WHERE id=?";
-			 
-			PreparedStatement statement = conexao.prepareStatement(sql);
+		try(
+				Connection conexao = ConexaoFactory.getConnection();
+				PreparedStatement statement = conexao.prepareStatement(sql);
+		) {
+
 			statement.setString(1, tarefa.getTitulo());
 			statement.setString(2, tarefa.getDescricao());
 			statement.setString(3, tarefa.getPrioridade());
